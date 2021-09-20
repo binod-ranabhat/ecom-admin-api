3a5000e50afc786e9b5e1a4fdf291b3dc75165b1
Binod Ranabhat <merobinod@gmail.com>
2021-09-21 00:20:01 +1000

    Project Configuration with prettier formating for quots, semicolon, and make use of express

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("The Backend API is been HIT for E-Shop");
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+//middleware
+app.use(helmet())
+app.use(cors())
+app.use(morgan())
+
+// router to handle root request
+app.use('/', (request, response) => {
+  response.send('You have reached the end of the router')
+})
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error)
+  }
+  console.log(
+    `\n----------------------------------------` +
+      `\n SERVER running at http://localhost:${PORT}` +
+      `\n----------------------------------------`
+  )
+})
